38b48e7822d6fe52bf1cd1a0d441dd58f0dcffe2
Jake Luecke <jrluecke95@gmail.com>
2021-04-21 11:23:51 -0400

    create plate route done

--- routes/plates.js
+++ routes/plates.js
@@ -8,31 +8,33 @@ router.post('/create', checkAuth, async (req, res) => {
   //check to make sure recipe has all fields filled out
   const { user } = req.session;
   // checks to see if fields are filled out
-  if (!req.body.name || !req.body.description || !req.body.price || req.body.quantity) {
+  if (!req.body.name || !req.body.description || !req.body.price || !req.body.quantity) {
     return res.status(400).json({
       error: 'please fill out all fields'
     })
   }
 // checks to make sure user doesnt create duplicate listings
-  const recipe = await models.Recipe.findOne({
+  const recipe = await models.Plate.findOne({
     where: {
       UserId: user.id,
-      title: req.body.title
+      name: req.body.name
     }
   })
   if (recipe) {
     return res.status(400).json({
-      error: "recipe title already exists"
+      error: "you already have this recipe listed!"
     })
   }
 // creates new recipe
-  const newRecipe = await models.Recipe.create({
-    title: req.body.title,
-    recipe: req.body.recipe,
+  const newPlate = await models.Plate.create({
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+    quantity: req.body.quantity,
     UserId: user.id,
   })
 
-  return res.status(201).json(newRecipe)
+  return res.status(201).json(newPlate)
 })
 
 module.exports = router;
