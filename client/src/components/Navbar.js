b47452e04a335551569f44f0c8450db2c2dda7dc
Andres Icedo <andresicedo24@gmail.com>
2021-04-21 13:29:01 -0500

    updtaed navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,19 +5,30 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import { Button } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    toolBar: {
+      backgroundColor: 'black'
+    }
+  });
 
 export default function Navbar() {
+    const classes = useStyles();
+
     return (
         <div>
             <AppBar position="static">
-                <Toolbar variant="dense">
+                <Toolbar classes={{
+                    root: classes.toolBar,
+                    }} variant="dense">
                     <IconButton edge="start" color="inherit" aria-label="menu">
                     </IconButton>
                     <Typography variant="h6" >
                         PlatePool
                     </Typography>
-                    <Button component={NavLink} to="/login" color="inherit">Login</Button>
-                    <Button component={NavLink} to="/register" color="inherit">Register</Button>
+                    <Button component={NavLink} style={{float: 'right'}} to="/login" color="inherit">Login</Button>
+                    <Button component={NavLink} style={{float: 'right'}} to="/register" color="inherit">Register</Button>
                 </Toolbar>
             </AppBar>
         </div>
