ab965a10557846f5d14075f083f840433dba0670
Kendall Oliver <kendall.oliver93@gmail.com>
2021-04-21 13:58:48 -0500

    test push

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Container } from "@material-ui/core";
 import Home from './pages/Home'
 import './App.css'
+import Login from "./pages/Login";
+import Register from './pages/Register'
+import Navbar from "./components/Navbar";
+import CreatePlate from "./pages/CreatePlate";
 
 
 
@@ -15,28 +15,20 @@ function App() {
     <div className="App">
 
       <Router>
-        <AppBar position="static">
-          <Toolbar variant="dense">          
-            <Typography variant="h6" color="inherit">
-              GreenLeaf
-            </Typography>
-              <>
-              <Button color="inherit" component={NavLink} to="/">
-                  Restaurants
-                </Button> 
-                <Button color="inherit" component={NavLink} to="/List">
-                  Order
-                </Button>
-                {/* <Button color="inherit" component={NavLink} to="/Add">
-                  button
-                </Button> */}
-              </>
-          </Toolbar>
-        </AppBar>
+        <Navbar />
         <Container style={{ margin: '2em auto' }}>
             <Switch>
               <Route path="/" exact>
                 <Home />
+              </Route>
+              <Route path='/register'>
+                <Register />
+              </Route>
+              <Route path='/login'>
+                <Login />
+              </Route>
+              <Route path='/CreatePlate'>
+                <CreatePlate />
               </Route>
               {/* <Route path="/#">
                 <Page />
