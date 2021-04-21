83836d7362add8d306c4a0c6cc54184d05bda8fd
Jake Luecke <jrluecke95@gmail.com>
2021-04-21 10:46:25 -0400

    finished register route

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const models = require('../models');
+const bcrypt = require('bcrypt');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// localhost:3000/api/v1/customers/register
+// localhost:3000/api/v1/users/register
 router.post('/register', async (req, res) => {
   // check for username and password
   if (!req.body.email || !req.body.password) {
@@ -15,17 +17,32 @@ router.post('/register', async (req, res) => {
     })
   }
   //check database for existing user
-  const customer = await models.Customer.findOne({
+  const user = await models.User.findOne({
     where: {
       email: req.body.email
     }
   })
   //if exists, send error
-  if (customer) {
+  if (user) {
     return res.status(400).json({
-      error: 'email is already associated with an account!'
+      error: 'user already exists'
     })
   }
+
+  // hash password
+  const hash = await bcrypt.hash(req.body.password, 10)
+  // create user
+  const newUser = await models.User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: hash,
+    street: req.body.street,
+    zipcode: req.body.zipcode,
+    city: req.body.zipcode,
+    state: req.body.state
+  })
+  // respond with success message
+  return res.status(201).json(newUser)
 })
 
 // localhost:3000/api/v1/users/login
@@ -57,7 +74,7 @@ router.post('/login', async (req, res) => {
     })
   }
   //store user info in session/log in 
-  req.session.customer = customer
+  req.session.user = user
   //respond with success
   return res.json({
     id: user.id,
