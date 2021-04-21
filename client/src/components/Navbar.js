9a656675ed8bb1c1dc36f9e8da237e8570d9d1fc
Andres Icedo <andresicedo24@gmail.com>
2021-04-21 13:13:44 -0500

    updated menu icons navbar js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import { Button } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
 
@@ -12,10 +11,9 @@ export default function Navbar() {
         <div>
             <AppBar position="static">
                 <Toolbar variant="dense">
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
+                    <IconButton edge="start" color="inherit" aria-label="menu">
                     </IconButton>
-                    <Typography variant="h6" className={classes.title}>
+                    <Typography variant="h6" >
                         PlatePool
                     </Typography>
                     <Button component={NavLink} to="/login" color="inherit">Login</Button>
