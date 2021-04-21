dc59541398ddf1f8914f8e266476ff71da380d63
Willie Rose <willierose87@gmail.com>
2021-04-21 15:09:08 -0500

    Made base model of create a plate page and hoooked it up to the backend

--- client/src/components/CreatePlateSection.js
+++ client/src/components/CreatePlateSection.js
@@ -1,23 +1,136 @@
-import { FormControl, Input, InputLabel, TextField } from '@material-ui/core';
-import React from 'react'
+import { Button, FormControl, Input, InputLabel, MenuItem, TextField } from '@material-ui/core';
+import React, { useState } from 'react'
 
 const CreatePlateSection = () => {
     // name, price, 
     // description, 
     // cuisine, quantity, alergenInfo
+
+    const [form, setForm] = useState({
+        name: '',
+        price: '',
+        description: '',
+        cuisine: '',
+        allergies: '',
+        quantity: ''
+    }) 
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch('/api/v1/plates/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: form.name,
+                price: form.price,
+                description: form.description,
+                cuisine: form.cuisine,
+                allergenInfo: form.allergenInfo,
+                quantity: form.quantity
+            }),
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.error) {
+                    alert(data.error)
+                } else {
+                    alert('User Created Meal Successfully')
+                }
+            })
+    }
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
+
+    const cuisines = [
+        {
+           name: 'Asian' 
+        },{
+            name: 'American'
+        },{
+            name: 'Italian'
+        },{
+            name: 'Mexican'
+        },{
+            name: 'Breakfast'
+        },{
+            name: 'French'
+        },{
+            name: 'African'
+        },{
+            name: 'BBQ'
+        },{
+            name: 'Brazillian'
+        },{
+            name: 'Jamaican'
+        }
+    ]
+
+    const quantities = [
+        {
+            number: 1
+        },{
+            number: 2
+        },{
+            number: 3
+        },{
+            number: 4
+        },{
+            number: 5
+        },{
+            number: 6
+        },{
+            number: 7
+        },{
+            number: 8
+        },{
+            number: 9
+        },{
+            number: 10
+        }
+    ]
+
     return (
         <div>
             <h1>Create A Plate</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <FormControl style={{marginRight: '5%', width: '40%'}}>
                     <InputLabel>Name</InputLabel>
-                    <Input />
-                    <TextField multiline row={4} style={{marginTop: '5%'}} />
+                    <Input onChange={handleChange} name="name" value={form.name} />
+                    <TextField onChange={handleChange} name="description" value={form.description} multiline row={4} style={{marginTop: '10%'}} placeholder="Description"/>
+                    <TextField onChange={handleChange} name="allergenInfo" value={form.allergenInfo} style={{marginTop: '10%'}} placeholder="Alergies" />
                 </FormControl>
                 <FormControl>
                     <InputLabel>$ Price</InputLabel>
-                    <Input />
+                    <Input onChange={handleChange} name="price" value={form.price} />
+                    <TextField onChange={handleChange} value={form.cuisine} name="cuisine" style={{marginTop: '20%'}} select id="standard-select-state" label="Cuisine">
+                        {
+                            cuisines.map((cuisine) => (
+                                <MenuItem key={cuisine.name} value={cuisine.name}>
+                                    {cuisine.name}
+                                </MenuItem>
+                            ))
+                        }
+                    </TextField>
+                    <TextField onChange={handleChange} name="quantity" value={form.quantity} style={{marginTop: '18%'}} select id="standard-select-state" label="quantity">
+                        {
+                            quantities.map((quantity) => (
+                                <MenuItem key={quantity.number} value={quantity.number}>
+                                    {quantity.number}
+                                </MenuItem>
+                            ))
+                        }
+                    </TextField>
                 </FormControl>
+                <br />
+                <Button type="submit" style={{ marginTop: '2%' }} variant="contained" color="primary">Create Meal!</Button>
             </form>
         </div>
     )
