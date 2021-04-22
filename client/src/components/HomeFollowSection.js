2fca78856447440d8ce5b76739819063a79063d5
Willie Rose <willierose87@gmail.com>
2021-04-22 14:27:18 -0500

    Added a file for right side of home page

---
+++ client/src/components/HomeFollowSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeFollowSection = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HomeFollowSection
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import { Grid } from '@material-ui/core'
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
