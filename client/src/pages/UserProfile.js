32edbc721c72961bd7e083e7114066b83d6466ee
Jake Luecke <jrluecke95@gmail.com>
2021-04-23 14:14:50 -0400

    fixed plates routes

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -2,6 +2,7 @@ import { Grid, Paper } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import { useSelector } from 'react-redux';
+import HomeFollowSection from '../components/RightContainer/HomeFollowSection'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,12 +18,14 @@ const useStyles = makeStyles((theme) => ({
 export const UserProfile = () => {
   const classes = useStyles();
   const user = useSelector((state) => state.user);
-  const [ plates, setPlates ] = useState('')
+  const [ plates, setPlates ] = useState('');
+
 
   useEffect(() => {
     fetch(`/${user.id}/getusersplates`)
     .then(res => res.json())
     .then(data => {
+      console.log(data)
       setPlates(data)
     })
   }, [])
@@ -34,11 +37,15 @@ export const UserProfile = () => {
         <Grid item xs={8}>
           <Paper className={classes.paper}>
             <h1>{user.name}</h1>
-
+            {/* {plates.map(plate => {
+              return (
+                <p>{plate}</p>
+              )
+            })} */}
             </Paper>
         </Grid>
         <Grid item xs={4}>
-          <Paper className={classes.paper}>Right</Paper>
+          <Paper className={classes.paper}><HomeFollowSection /></Paper>
         </Grid>
       </Grid>
       ) : (
