6d840e843507466e9f5cc69121f635488a177828
Willie Rose <willierose87@gmail.com>
2021-04-29 10:32:46 -0500

    Got other dishes by user and comments to show up on post page

--- client/src/pages/SearchPost.js
+++ client/src/pages/SearchPost.js
@@ -1,15 +1,29 @@
 import { Grid } from '@material-ui/core';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router';
 import PostSection from '../components/PostSection'
 import SearchPostComments from '../components/SearchPostComments';
 
 
 
 const SearchPost = () => {
+    const [data, setData] = useState(null)
+    const {id} = useParams()
+
+    useEffect(() => {
+        fetch(`/api/v1/plates/${id}/plate`)
+            .then(res => res.json())
+            .then(data => {
+                setData(data)
+                // console.log(data.UserId)
+            })
+    },[id])
+
+
     return (
         <Grid container spacing={2}>
-            <PostSection />
-            <SearchPostComments />
+            <PostSection plate={data}/>
+            <SearchPostComments plate={data}/>
         </Grid>
     )
 }
